88b9559278e2b173b9f7242c3b11194c93fe0b3a
Benson <53087589+bensonmk@users.noreply.github.com>
2026-07-19 19:39:53 +0530

    feat: add BCrypt password hashing with Spring Security

--- src/main/java/com/bmk/nexus/service/UserService.java
+++ src/main/java/com/bmk/nexus/service/UserService.java
@@ -5,6 +5,7 @@ import com.bmk.nexus.entity.User;
 import com.bmk.nexus.exception.EmailAlreadyExistsException;
 import com.bmk.nexus.mapper.UserMapper;
 import com.bmk.nexus.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,15 +13,20 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository, UserMapper userMapper) {
+    public UserService(UserRepository userRepository, UserMapper userMapper, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public User createUser(UserRequestDto userRequestDto) {
 
+
         User user = userMapper.toEntity(userRequestDto);
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
 
         if (userRepository.existsByEmail(userRequestDto.getEmail())) {
             throw new EmailAlreadyExistsException("Email already exists");
